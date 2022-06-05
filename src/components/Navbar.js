85de6a52747e53cf72faa81adb841124d6838501
Tayomide <tibrahim@hawk.iit.edu>
2022-06-05 03:06:32 -0500

    Define layout of Navbar component
    Co-Authored-By: Joshtray <23462200+Joshtray@users.noreply.github.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,34 @@ import styled from 'styled-components'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <NavbarContainer>
+        <h1>
+            <Link to="#">P Beam</Link>
+        </h1>
+        <ol>
+            <li>
+                <Link to="#">Favorite</Link>
+            </li>
+            <li>
+                <Link to="#">Add House</Link>
+            </li>
+            <li>
+                <Link to="#">Account</Link>
+                <ol>
+                    <li>
+                        <Link to="#">Account Settings</Link>
+                    </li>
+                    <li>
+                        <Link to="#">Upgrade To Seller</Link>
+                    </li>
+                </ol>
+            </li>
+        </ol>
+        {/* Home page */}
+        {/* Favorite */}
+        {/* Add housing info for seller */}
+        {/* Account Switch /  Show account status too */}
+    </NavbarContainer>
   )
 }
 
