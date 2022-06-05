67c5ac20dc141884233ea461a49cd96dacea01d1
Tayomide <tibrahim@hawk.iit.edu>
2022-06-05 00:59:27 -0500

    Import needed components
    Co-Authored-By: Joshtray <23462200+Joshtray@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
