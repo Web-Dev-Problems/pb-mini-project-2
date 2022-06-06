560038ee77a2ae21dd280e23d0e9dac3edff8400
Tayomide <tibrahim@hawk.iit.edu>
2022-06-06 06:58:50 -0500

    Import packages for Home Component
    Co-Authored-By: Joshtray <23462200+Joshtray@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
-import React from 'react'
+// import Filter from '../components/Filter'
+import HomeBlock from '../components/HomeBlock';
+import styled from "styled-components"
 
 function Home() {
   return (
