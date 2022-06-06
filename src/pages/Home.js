c500ce583c8b69fbdb8d2704204292bf554f812f
Tayomide <tibrahim@hawk.iit.edu>
2022-06-06 06:59:47 -0500

    Map data into individual Home block component
    Co-Authored-By: Joshtray <23462200+Joshtray@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,14 @@ import styled from "styled-components"
 
 function Home({ houseData, setFavorite, setHouseData }) {
   return (
-    <div>Home</div>
+    <HomeContainer>
+        {/* <Filter /> */}
+        <ul>
+            {houseData && Object.values(houseData).map((value, i) => {
+                return <HomeBlock key={i} value={value} index={i} setFavorite={setFavorite} setHouseData={setHouseData} />
+            })}
+        </ul>
+    </HomeContainer>
   )
 }
 
