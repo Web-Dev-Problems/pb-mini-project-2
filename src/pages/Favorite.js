58b171fcc8eb86bb7cc9dc9312426e81d041eae2
Tayomide <tibrahim@hawk.iit.edu>
2022-06-06 12:25:32 -0500

    Change grid layout

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -23,7 +23,7 @@ const FavoriteContainer = styled.section`
     flex-direction: column;
     > ul:nth-of-type(1){
         display: grid;
-        grid-template-columns: repeat(4, 300px);
+        grid-template-columns: repeat(auto-fill, 300px);
         margin: 0 auto;
         grid-column-gap: 34px;
         grid-row-gap: 27px;
