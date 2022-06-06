108a5a47680e69a1245da90cfa7d8ad071b34a0d
Tayomide <tibrahim@hawk.iit.edu>
2022-06-06 06:58:17 -0500

    Pass props into Favorite Component
    Co-Authored-By: Joshtray <23462200+Joshtray@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -96,6 +96,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home houseData = {houseData} setFavorite={setFavorite} setHouseData={setHouseData} />} />
+          <Route exact path="/favorite" element={<Favorite houseData = {houseData} favorite={favorite} setFavorite={setFavorite} setHouseData={setHouseData} />} />
           {/* Page for each product like https://www.tesla.com/model3 */}
           {/* Before they compare we should ask for their optimal zip code or address so we could show how near a home is to there */}
         </Routes>
