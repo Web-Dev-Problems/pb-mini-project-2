ba956b93a1f8cb1200cdd8731e176dde5ce28731
Tayomide <tibrahim@hawk.iit.edu>
2022-06-06 22:30:25 -0500

    Finish the structure of filter object
    Co-Authored-By: Joshtray <23462200+Joshtray@users.noreply.github.com>

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -22,13 +22,15 @@ function Filter() {
                 placeholder : "$",
                 placeholderPosition: "left",
                 values : [10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000],
-                valueIndex : 8
+                valueIndex : 7
             },
             priceSort : {
-                // Sort price up or down
+                type : "sort",
+                text : "Price",
             },
             propertyType : {
                 type : "dropdown",
+                text : "Housing Type: ",
                 placeholder : "home",
                 placeholderPosition: "right",
                 values : ["All", "Apartment", "Single-Family", "Condo", "Townhouse", "Multi-Family", "Co-op", "Commercial"],
@@ -42,7 +44,10 @@ function Filter() {
                 values :[2022, 2020, 2018, 2016, 2014, 2012, 2010, 2008, 2006, 2004, 2002, 2000],
                 valueIndex : 0
             },
-            yearSort : {},
+            yearSort : {
+                type : "sort",
+                text : "Year",
+            },
             beds : {
                 type : "dropdownInput",
                 text : "Contains at least ",
@@ -53,7 +58,10 @@ function Filter() {
                 valueIndex : 0
 
             },
-            bedSort : {},
+            bedSort : {
+                type : "sort",
+                text : "Bedrooms",
+            },
             baths : {
                 type : "dropdownInput",
                 text : "Contains at least ",
@@ -63,37 +71,57 @@ function Filter() {
                 values : [0, 1, 2, 3],
                 valueIndex : 0
             },
-            bathSort : {},
+            bathSort : {
+                type : "sort",
+                text : "Bathrooms",
+            },
             minsqFt : {
                 type : "dropdownInput",
                 text: "Minimum Sq Ft of ",
                 placeholder : "sq. ft",
-                placeholderPosition: "left"
+                placeholderPosition: "left",
+                values: [0, 1000, 2000, 3000, 4000, 4100, 4500, 5000, 5500, 6500],
+                valueIndex: 0
             },
             maxsqFt : {
                 type : "dropdownInput",
                 text: "Maximum Sq Ft of ",
                 placeholder : "sq. ft",
-                placeholderPosition: "left"
+                placeholderPosition: "left",
+                values: [5000, 10000, 20000, 30000, 40000, 41000, 45000, 50000, 55000, 65000],
+                valueIndex: 0
+            },
+            sqFtSort : {
+                type : "sort",
+                text : "SQ Ft",
             },
             zip : {
                 type : "dropdownInput",
                 text : "Zip Code: ",
                 placeholder : "",
-                placeholderPosition: "left"
-
+                placeholderPosition: "left",
+                values : [60000, 60001, 60002, 60003, 60004, 60005, 60006, 60007, 60008],
+                valueIndex : 0,
             },
             minPriceSqFt : {
                 type : "dropdownInput",
                 text : "Minimum psf of ",
                 placeholder : "psf",
-                placeholderPosition: "right"
+                placeholderPosition: "right",
+                values : [0, 1000, 2000, 3000, 4000, 6000, 8000, 10000, 15000],
+                valueIndex : 0
             },
             maxPriceSqFt : {
                 type : "dropdownInput",
                 text : "Maximum psf of ",
                 placeholder : "psf",
-                placeholderPosition: "right"
+                placeholderPosition: "right",
+                values : [10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000],
+                valueIndex : 7
+            },
+            priceSQFtSort : {
+                type : "sort",
+                text : "Price Sq Ft",
             }
         }
     )
